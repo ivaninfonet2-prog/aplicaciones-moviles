06cfac2938fac9a0ff3f12d4b3702f292ab8f374
Ivan Infonet <ivaninfonet2@gmail.com>
2026-01-03 01:28:49 -0300

    Actualizando proyecto php_2026 (commit vacío)

--- application/views/registrar_usuario/body_registrar_usuario.php
+++ application/views/registrar_usuario/body_registrar_usuario.php
@@ -11,31 +11,6 @@
 
 <body>
 
-<header class="main-header">
-
-    <div class="header-container">
-
-        <!-- Logo + título -->
-        <a href="<?= base_url(); ?>" class="brand">
-            <img
-                src="<?= base_url('activos/imagenes/logo.jpg'); ?>"
-                class="logo-img"
-                alt="Logo UNLa Tienda"
-            >
-            <span class="site-title">UNLa Tienda</span>
-        </a>
-
-        <!-- Botón a la derecha -->
-        <nav class="nav-menu">
-            <a href="<?= base_url(); ?>" class="btn btn-login">
-                Ir al Inicio
-            </a>
-        </nav>
-
-    </div>
-
-</header>
-
 <main class="registro-container" style="background-image: url('<?= $fondo; ?>');">
 
     <!-- Texto encima de la tarjeta -->
