01aa2963eeafbc1fbd1e8c17aa40285133e6dc7f
Ivan Infonet <ivaninfonet2@gmail.com>
2026-01-15 20:19:01 -0300

    Actualizando proyecto php_2026 (commit vacío)

--- mi_base.sql
+++ mi_base.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-01-2026 a las 19:21:04
+-- Tiempo de generación: 16-01-2026 a las 00:14:19
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 7.4.30
 
@@ -32,64 +32,6 @@ CREATE TABLE `clientes` (
   `usuario_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `clientes`
---
-
-INSERT INTO `clientes` (`id_cliente`, `usuario_id`) VALUES
-(1, 1),
-(2, 1),
-(3, 1),
-(4, 1),
-(5, 1),
-(6, 1),
-(29, 1),
-(30, 1),
-(31, 1),
-(32, 1),
-(33, 1),
-(34, 1),
-(35, 1),
-(36, 1),
-(37, 1),
-(38, 1),
-(39, 1),
-(40, 1),
-(41, 1),
-(42, 1),
-(43, 1),
-(44, 1),
-(45, 1),
-(46, 1),
-(47, 1),
-(48, 1),
-(49, 1),
-(50, 1),
-(51, 1),
-(52, 1),
-(53, 1),
-(54, 1),
-(55, 1),
-(56, 1),
-(57, 1),
-(58, 1),
-(59, 1),
-(60, 1),
-(61, 1),
-(62, 1),
-(63, 1),
-(64, 1),
-(65, 1),
-(66, 1),
-(27, 2),
-(21, 11),
-(22, 11),
-(23, 11),
-(24, 11),
-(25, 11),
-(26, 11),
-(28, 11);
-
 -- --------------------------------------------------------
 
 --
@@ -113,10 +55,8 @@ CREATE TABLE `espectaculos` (
 --
 
 INSERT INTO `espectaculos` (`id_espectaculo`, `nombre`, `descripcion`, `disponibles`, `precio`, `direccion`, `fecha`, `hora`, `imagen`) VALUES
-(20, 'maria becerra', 'la nena de argentina', 867, '77777.00', 'belgrano 4444', '2025-12-31', '22:11:00', '62df21eab7af8c186e82fddda3749853.jpg'),
-(25, 'duki', 'el mejor del mundo', 8777, '55444.00', 'eva peron 333', '2026-01-22', '22:11:00', 'd94ab42aba3969511412a6dc0af180ea.jpg'),
-(26, 'emilia mernes', 'la princesa del mundo', 2222, '33333.00', 'camino negro 3333, villa centenario', '2026-01-27', '21:12:00', '7da1571f450cb4591ca34fecefba8b5f.jpg'),
-(27, 'nicky nicole', 'la rapera del mundo', 88888, '77777.00', 'lanus 22111', '2026-01-06', '22:13:00', '91d393c44ddc56c1d9fc4f5a4eb2e5c5.jpg');
+(20, 'maria becerra ttttttt', 'la nena de argentina hhhhhhh', 865, '77777.00', 'belgrano 44445555', '2025-12-31', '22:11:00', '62df21eab7af8c186e82fddda3749853.jpg'),
+(29, 'emilia mernes la mejor', 'ggg', 55555, '66666.00', 'belgrano 4444', '2026-01-22', '19:48:00', '55fcce243cb3b9ccef0a265e0b845d5f.jpg');
 
 -- --------------------------------------------------------
 
@@ -133,16 +73,6 @@ CREATE TABLE `reservas` (
   `monto_total` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `reservas`
---
-
-INSERT INTO `reservas` (`id_reserva`, `usuario_id`, `espectaculo_id`, `cantidad`, `fecha_reserva`, `monto_total`) VALUES
-(64, 1, 20, 3, '2026-01-12', 233331),
-(65, 1, 20, 3, '2026-01-12', 233331),
-(66, 1, 20, 4, '2026-01-12', 311108),
-(67, 1, 20, 2, '2026-01-13', 155554);
-
 -- --------------------------------------------------------
 
 --
@@ -151,7 +81,7 @@ INSERT INTO `reservas` (`id_reserva`, `usuario_id`, `espectaculo_id`, `cantidad`
 
 CREATE TABLE `roles` (
   `id_rol` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL
+  `nombre` varchar(51) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -159,8 +89,8 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id_rol`, `nombre`) VALUES
-(1, 'administrador'),
-(2, 'usuario');
+(1, 'usuario'),
+(2, 'administrador');
 
 -- --------------------------------------------------------
 
@@ -171,12 +101,12 @@ INSERT INTO `roles` (`id_rol`, `nombre`) VALUES
 CREATE TABLE `usuarios` (
   `id_usuario` int(11) NOT NULL,
   `rol_id` int(11) NOT NULL,
-  `nombre_usuario` varchar(100) NOT NULL,
-  `palabra_clave` varchar(255) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `apellido` varchar(50) NOT NULL,
+  `nombre_usuario` varchar(51) NOT NULL,
+  `palabra_clave` varchar(51) NOT NULL,
+  `nombre` varchar(51) NOT NULL,
+  `apellido` varchar(51) NOT NULL,
   `dni` int(11) NOT NULL,
-  `telefono` varchar(20) NOT NULL
+  `telefono` varchar(21) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -184,16 +114,51 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `rol_id`, `nombre_usuario`, `palabra_clave`, `nombre`, `apellido`, `dni`, `telefono`) VALUES
-(1, 2, 'ivaninfonet@gmail.com', '1234', 'ivan', 'tolaba', 34571058, '11-2310-6932'),
-(2, 2, 'ivaninfonet2@gmail.com', '1234', 'pablo', 'flores', 42589521, '11-4082-6777'),
 (31, 1, 'ivaninfonorte@gmail.com', '1234', 'gerardo', 'fernandez', 34234433, '11-4156-3813'),
 (32, 1, 'ivaninfosur@gmail.com', '1234', 'miguel', 'angel', 2147483647, '11-2310-8788'),
 (33, 1, 'ivaninfoeste@gmail.com', '1234', 'rodrigo', 'gomez', 12345678, '4444-4444'),
-(34, 1, 'ivaninfoeste2@gmail.com', '1234', 'rodrigo', 'flores', 3222114, '5555-6666');
+(34, 1, 'ivaninfoeste2@gmail.com', '1234', 'rodrigo ', 'flores', 3222114, '5555-6666'),
+(36, 2, 'ivaninfonet@gmail.com', '1234', 'ivan', 'tolaba', 2211112, '5555-6666'),
+(37, 2, 'ivaninfonet2@gmail.com', '1234', 'pablo', 'flores', 11111111, '4444-5555');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `id_venta` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `espectaculo_id` int(11) NOT NULL,
+  `fecha_venta` date NOT NULL,
+  `monto_total` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id_cliente`),
+  ADD KEY `usuario` (`usuario_id`);
+
+--
+-- Indices de la tabla `espectaculos`
+--
+ALTER TABLE `espectaculos`
+  ADD PRIMARY KEY (`id_espectaculo`);
+
+--
+-- Indices de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  ADD PRIMARY KEY (`id_reserva`),
+  ADD KEY `espectaculos` (`espectaculo_id`),
+  ADD KEY `usuarios` (`usuario_id`);
 
 --
 -- Indices de la tabla `roles`
@@ -206,12 +171,37 @@ ALTER TABLE `roles`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id_usuario`),
-  ADD UNIQUE KEY `nombre_usuario` (`nombre_usuario`),
-  ADD KEY `fk_usuario_rol` (`rol_id`);
+  ADD KEY `rol` (`rol_id`);
+
+--
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`id_venta`),
+  ADD KEY `espectaculo` (`espectaculo_id`),
+  ADD KEY `reserva` (`usuario_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+
+--
+-- AUTO_INCREMENT de la tabla `espectaculos`
+--
+ALTER TABLE `espectaculos`
+  MODIFY `id_espectaculo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
@@ -223,7 +213,42 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD CONSTRAINT `usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Filtros para la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  ADD CONSTRAINT `espectaculos` FOREIGN KEY (`espectaculo_id`) REFERENCES `espectaculos` (`id_espectaculo`),
+  ADD CONSTRAINT `usuarios` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `rol` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`id_rol`);
+
+--
+-- Filtros para la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD CONSTRAINT `espectaculo` FOREIGN KEY (`espectaculo_id`) REFERENCES `espectaculos` (`id_espectaculo`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
